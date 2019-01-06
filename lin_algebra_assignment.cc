2ac772ae5b897c26c6b5560057c7bafa6c0bb81a
Lukas Olson <lukasdragon@live.ca>
2019-01-05 18:25:57 -0800

    Finished the assignment

--- lin_algebra_assignment.cc
+++ lin_algebra_assignment.cc
@@ -1,78 +1,104 @@
-// Mr. Arkiletian ICTP 12
-// Simultaneous linear equation assignment (2 team members)
-// Solved using the STL generic vector container class 
-
 #define _CRT_SECURE_NO_DEPRECATE
 #include <iostream>
 #include <vector>
 #include <cmath> // for pow function
 
-using namespace std;
+//NOTE TO MR. ARK https://stackoverflow.com/questions/1452721/why-is-using-namespace-std-considered-bad-practice
 
-typedef vector <double> Dvector;
-typedef vector <Dvector> Dmatrix;
+typedef std::vector <double> dvector;
+typedef std::vector <dvector> dmatrix;
 
-// Prototypes (variable names included, but not needed)
-// DO NOT change any prototypes!! NO other functions allowed.
 
-Dmatrix GetA();
-// gets the matrix A from the user and returns it
+/**
+ * \brief Gets the A matrix from the user.
+ * \return A-dmatrix.
+ */
+dmatrix get_a();
 
-Dvector Getb(const int &siz);
-// gets the vector b from the user and returns it
+/**
+ * \brief Gets the B vector from the user.
+ * \param size Size of the expected vector.
+ * \return  B-Dvector
+ */
+dvector get_b(const int & size);
 
-void Display(const Dvector &xvect);
-// displays the contents of vector xvect
 
-void Display(const Dmatrix &Amat);
-// displays the contents of matrix Amat (Note: Display function is overloaded)
+/**
+ * \brief Displays the contents of vector x_vector.
+ * \param x_vector dvector to display.
+ */
+void display(const dvector & x_vector);
 
-Dvector Solvex(const Dmatrix &Amat, const Dvector &bvect);
-// Solves for systems of linear equations in the form Ax=b using cramer's rule 
 
-double Det(const Dmatrix &Amat);
-// returns the Determinant of matrix Amat using a recursive algorithm
-// base case is 2x2 matrix 
+/**
+ * \brief displays the contents of matrix a_matrix (Note: display function is overloaded)
+ * \param a_matrix dmatrix to display.
+ */
+void display(const dmatrix & a_matrix);
 
-Dmatrix Replace(Dmatrix Rmat, const int col, const Dvector &bvect);
-// Replaces the column 'col' in Rmat with bvect and returns Rmat
 
-Dmatrix Minor(const Dmatrix &Amat, const int col);
-// Gets the Minor matrix of Amat by crossing out the first row and 
-// column 'col' then returns that smaller matrix
+/**
+ * \brief Solves for systems of linear equations in the form Ax=b using Cramer's rule.
+ * \param a_matrix Input matrix.
+ * \param b_vector Known values.
+ * \return returns unknown dvector X
+ */
+dvector solve_x(const dmatrix & a_matrix, const dvector & b_vector);
+
+
+/**
+ * \brief Uses recursion to determine the determinant of a matrix. Base case is 2x2.
+ * \param a_matrix Input matrix.
+ * \return The Determinant of a_matrix.
+ */
+double det(const dmatrix & a_matrix);
+
+
+/**
+ * \brief Replaces a column with a vector.
+ * \param replace_matrix The matrix to replace a column in.
+ * \param col Column position.
+ * \param b_vector What to replace with.
+ * \return Matrix with the replaced column.
+ */
+dmatrix replace(dmatrix replace_matrix, int col, const dvector & b_vector);
+
+
+/**
+ * \brief Gets the minor matrix of a_matrix by crossing out the first row and a column position.
+ * \param a_matrix The matrix to perform the operation on.
+ * \param col Column position.
+ * \return The smaller matrix
+ */
+dmatrix minor(const dmatrix & a_matrix, int col);
 
 
 //-------------------------------------------
 int main() {
 
-	Dmatrix A;
-	Dvector b, x;
+	auto a_matrix = get_a();
+	display(a_matrix);
+	const auto b_vector = get_b(a_matrix.size());
+	const auto x_vector = solve_x(a_matrix, b_vector);
+	display(x_vector);
 
-	A = GetA();
-	Display(A);
-	b = Getb(A.size());
-	x = Solvex(A, b);
-	Display(x);
-
-	system("PAUSE");
-
-	return(0);
+	return (0);
 }
 //-------------------------------------------
 
-Dmatrix GetA() {
-	cout << "Please input Columns A. Format: <Matrix Size> <A11> <A12> <A13> etc." << endl;
+dmatrix get_a() {
+	std::cout << "Please input Columns A. Format: <Matrix Size> <A11> <A12> <A13> etc." << std::endl;
 	int index;
 	scanf("%d", &index);
-	Dmatrix matrix = Dmatrix();
+	auto matrix = dmatrix();
 	matrix.reserve(index);
 
-	for (int i = 0; i < index; i++)
+	for (auto i = 0; i < index; i++)
 	{
-		Dvector row = Dvector();
+		auto row = dvector();
 		row.reserve(index);
 
-		for (int i = 0; i < index; i++)
+		for (auto o = 0; o < index; o++)
 		{
 			double elem = 0;
 			scanf("%lf", &elem);
@@ -83,10 +109,10 @@ Dmatrix GetA() {
 	return matrix;
 }
 
-Dvector Getb(const int & siz) {
-	cout << "Please input Column B. Format: <b1> <b2> <b3> etc." << endl;
-	Dvector vec = Dvector();
-	for (int i = 0; i < siz; i++)
+dvector get_b(const int & size) {
+	std::cout << "Please input Column B. Format: <b1> <b2> <b3> etc." << std::endl;
+	auto vec = dvector();
+	for (auto i = 0; i < size; i++)
 	{
 		double elem = 0;
 		scanf("%lf", &elem);
@@ -95,44 +121,74 @@ Dvector Getb(const int & siz) {
 	return vec;
 }
 
-void Display(const Dvector & xvect) {
-	cout << endl;
-	for (size_t i = 0; i < xvect.size(); i++)
+void display(const dvector & x_vector) {
+	std::cout << std::endl;
+	for (auto i : x_vector)
 	{
-		cout << xvect[i] << " ";
-		cout << endl;
+		std::cout << i << " " << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+void display(const dmatrix & a_matrix) {
+	std::cout << std::endl;
+	for (const auto & i : a_matrix)
+	{
+		for (auto j : i)
+			std::cout << j << " ";
+		std::cout << std::endl;
 	}
 }
 
-void Display(const Dmatrix & Amat) {
-	cout << endl;
-	for (size_t i = 0; i < Amat.size(); i++)
+dvector solve_x(const dmatrix & a_matrix, const dvector & b_vector) {
+	auto solution = dvector();
+	for (size_t i = 0; i < a_matrix.size(); i++)
 	{
-		for (size_t j = 0; j < Amat[i].size(); j++)
-			cout << Amat[i][j] << " ";
-		cout << endl;
+		solution.push_back(det(replace(a_matrix, i, b_vector)) / det(a_matrix));
 	}
+	return solution;
 }
 
-Dvector Solvex(const Dmatrix & Amat, const Dvector & bvect) {
-	return Dvector();
-}
-
-double Det(const Dmatrix & Amat) {
-	return 0.0;
-}
-
-// Replaces the column 'col' in Rmat with bvect and returns Rmat
-Dmatrix Replace(Dmatrix Rmat, const int col, const Dvector & bvect) {
-
-	for (size_t i = 0; i < Rmat.size(); i++)
+double det(const dmatrix & a_matrix) {
+	auto det_value = 0.0;
+	if (a_matrix.size() == 1)
 	{
-		Rmat[i].at(col) = bvect[i];
+		return a_matrix[0][0];
 	}
-	return Rmat;
+	if (a_matrix.size() == 2)
+	{
+		det_value = a_matrix[0][0] * a_matrix[1][1] - a_matrix[0][1] * a_matrix[1][0];
+		return det_value;
+	}
+	for (size_t p = 0; p < a_matrix[0].size(); p++)
+	{
+		det_value = det_value + a_matrix[0][p] * pow(-1, p) * det(minor(a_matrix, p));
+	}
+	return det_value;
 }
 
-Dmatrix Minor(const Dmatrix & Amat, const int col)
-{
-	return Dmatrix();
+dmatrix replace(dmatrix replace_matrix, const int col, const dvector & b_vector) {
+	for (size_t i = 0; i < replace_matrix.size(); i++)
+	{
+		replace_matrix[i].at(col) = b_vector[i];
+	}
+	return replace_matrix;
+}
+
+dmatrix minor(const dmatrix & a_matrix, const int col) {
+	dmatrix temp_matrix;
+	for (auto i = 1; i < a_matrix.size(); i++)
+	{
+		dvector temp_row;
+		for (auto j = 0; j < a_matrix[i].size(); j++)
+		{
+			if (j != col)
+			{
+				temp_row.push_back(a_matrix[i][j]);
+			}
+		}
+		if (!temp_row.empty())
+			temp_matrix.push_back(temp_row);
+	}
+	return temp_matrix;
 }
